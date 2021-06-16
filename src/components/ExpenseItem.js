d97fa35c2d01fe73fdec41761a0806f7a00001ad
Anoop S Mohan <anoop4439@gmail.com>
2021-06-16 20:34:56 +0530

    Expense item modified with values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 5, 9);
+    const expenseTitle = 'LIC';
+    const expenseAmount = '12,619';
     return (
         <div className="expense-item">
-            <div>June 9, 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">₹{expenseAmount}</div>
             </div>
         </div>
     )
